7b74afa5294f7ea7b26966d2258001ad67be96c2
nevyk <petr.nevyhosteny@gmail.com>
2014-06-04 19:46:06 +0000

    modify test folder to have template file for specs

---
+++ test/spec.js
@@ -0,0 +1,9 @@
+var expect = require('expect.js');
+
+describe('Top level', function() {
+    describe('Sub level', function() {
+        it('should help you to kickstart your awesome library', function () {
+            expect(true).to.be(true);
+        });
+    });
+});
